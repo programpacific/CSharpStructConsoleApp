6d0d168c1f3ae1d3e24c943ae4df27bb2883a02b
programpacific <programpacific@users.noreply.github.com>
2018-08-02 12:56:45 -0600

    Code completed.

--- CSharpStructConsoleApp/Program.cs
+++ CSharpStructConsoleApp/Program.cs
@@ -7,10 +7,22 @@ using System.Threading.Tasks;
 namespace CSharpStructConsoleApp
 {
     class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
+        Console.WriteLine("Welcome to the \"Struct\" Demonstration Application!");
+        Console.WriteLine("This brief example shows a struct in action! Thank you for demoing my application!");
+        Console.WriteLine();
 
-        }
+        Number number = new Number() { Amount = 3.4444m };
+        Console.WriteLine("Here is our decimal being housed inside of our \"Number\" struct: " + number.Amount);
+        Console.WriteLine("Press enter to exit!");
+        Console.ReadLine();
+    }
+}
+
+    public struct Number
+    {
+        public decimal Amount { get; set; }
     }
 }
